41ad9801232691d4845a3222b30896c3b8129d51
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-02-10 18:13:23 +0900

    Refact: timer 리팩토링
    - SetTimeOut이 매초마다 등록되는 방식에서 setInterval로 변경
    - isPlaying 변수로 time > 0 인지 체크
    - timer가 정의된 useEffect에 time 뎁스를 해제

--- my-app/src/components/App.tsx
+++ my-app/src/components/App.tsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Board from './Board';
-import { TIME_PER_STAGE } from '../global';
+import { TIME_PER_STAGE, MAX_STAGE } from '../global';
 
 function App() {
   const [state, setState] = useState({
+    isPlaying: true,
     stage: 1,
     time: TIME_PER_STAGE,
     score: 0,
   });
 
-  const { stage, time, score } = state;
+  const { isPlaying, stage, time, score } = state;
 
   const goNextStage = useCallback(() => {
-    setState(({ stage, time, score }) => ({
+    setState(({ stage, time, score, ...rest }) => ({
       stage: stage + 1,
       time: TIME_PER_STAGE,
       score: score + Math.pow(stage, 3) * time,
+      ...rest,
     }));
   }, []);
 
@@ -25,20 +27,26 @@ function App() {
 
   useEffect(() => {
     const countDown = () => {
-      if (time > 0) {
+      if (isPlaying) {
         setState(({ time, ...rest }) => ({ time: time - 1, ...rest }));
       } else {
         alert(`GAME OVER!\n스테이지: ${stage}, 점수: ${score}`);
-        setState({ stage: 1, time: TIME_PER_STAGE, score: 0 });
+        setState({ stage: 1, time: TIME_PER_STAGE, score: 0, isPlaying: true });
       }
     };
 
-    const timer = setTimeout(countDown, 1000);
+    const timer = setInterval(countDown, 1000);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [score, stage, time]);
+  }, [isPlaying, score, stage]);
+
+  useEffect(() => {
+    if (time <= 0) {
+      setState((prev) => ({ ...prev, isPlaying: false }));
+    }
+  }, [time]);
 
   return (
     <>
